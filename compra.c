ecd982ff5d8184ca068751f2490860abf66f7ba9
Your Name <you@example.com>
2018-11-28 18:44:35 -0300

    foi o itacir que fez

---
+++ compra.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+double compraComDesconto(int preco, int quantidade, double desconto) {
+  return 0.0;
+}
+
+int main () {
+	int preco = 20;
+	int quantidade = 40;
+	double desconto = 0.95;
+
+	double valorFinal = compraComDesconto(preco, quantidade, desconto); 
+
+	printf("O valor final é %f\n", valorFinal);
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
